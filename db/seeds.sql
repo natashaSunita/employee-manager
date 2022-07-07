5f393a6dc143166903da473cd2ec80d7fd1e679a
Natasha Beddow <beddow90@gmail.com>
2022-07-07 22:27:50 +0100

    populated schema

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO students (first_name, last_name, enrolled)
+VALUES ("Elliot", "Smith", false),
+       ("Amira", "Afzal", true),
+       ("Christoper", "Lee", true),
+       ("Verónica", "Rodriguez", false),
+       ("Igor", "Ivanov", true);
